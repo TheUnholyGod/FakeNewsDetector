67e6419cc40ed29c1f73c3b47db538eec7d9c494
TheUnholyGod <kitsonlow99@gmail.com>
2018-07-14 04:40:27 +0800

    lmostdone
    lmostdone

--- FakeNewsDetectorApp/FakeNewsDetectorApp/Forms/Form1.cs
+++ FakeNewsDetectorApp/FakeNewsDetectorApp/Forms/Form1.cs
@@ -41,14 +41,35 @@ namespace FakeNewsDetectorApp
             ArticleInfo[] checkerinfo = new ArticleInfo[urls.Length];
             int i = 0;
             richTextBox1.Text = baseinfo.All;
+
+            int positive_article = 0, negative_article = 0;
+            float percentage = 0;
+
             foreach (string url in urls)
             {
                 checkerinfo[i] = HtmlRemoval.ExtractInfo(url);
+
+                if (Program.b_brain.Result(checkerinfo[i].Articleinfo))
+                {
+                    ++positive_article;
+                }
+                else
+                {
+                    ++negative_article;
+                }
+
                 richTextBox1.Text += checkerinfo[i].All;
                 ++i;
             }
 
-
+            if(Program.b_brain.Result(baseinfo.Articleinfo))
+            {
+                percentage = ((float)positive_article / (float)(positive_article + negative_article)) * 100f;
+            }
+            else
+            {
+                percentage = ((float)negative_article / (float)(positive_article + negative_article)) * 100f;
+            }
 
         }
 
@@ -66,17 +87,22 @@ namespace FakeNewsDetectorApp
         {
             string Text = richTextBox2.Text;
 
+            richTextBox2.Text += "\n" + Program.b_brain.Result(richTextBox2.Text).ToString();
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            string Text = richTextBox2.Text;
+            Program.b_brain.Store(richTextBox2.Text, 'y');
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            string Text = richTextBox2.Text;
 
+            Program.b_brain.Store(richTextBox2.Text, 'n');
         }
 
         //private void Thesaurus_Click(object sender, EventArgs e)
