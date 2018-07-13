200e67ffd1e10745185171948b9a432c44543aa8
Kevin <Cjh2806@gmail.com>
2018-07-14 02:21:36 +0800

    Tabs and ToolStrip
    CHANGELOG:
    - Tabs for different functionality
    - Toolstrip for customization

--- FakeNewsDetectorApp/FakeNewsDetectorApp/Forms/Form1.Designer.cs
+++ FakeNewsDetectorApp/FakeNewsDetectorApp/Forms/Form1.Designer.cs
@@ -28,28 +28,40 @@ namespace FakeNewsDetectorApp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Search = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.Thesaurus = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.tEstToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(72, 132);
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            this.textBox1.Location = new System.Drawing.Point(66, 45);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(524, 22);
+            this.textBox1.Size = new System.Drawing.Size(394, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Search
             // 
             this.Search.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.Search.Location = new System.Drawing.Point(296, 193);
+            this.Search.Location = new System.Drawing.Point(237, 120);
+            this.Search.Margin = new System.Windows.Forms.Padding(2);
             this.Search.Name = "Search";
-            this.Search.Size = new System.Drawing.Size(76, 23);
+            this.Search.Size = new System.Drawing.Size(58, 19);
             this.Search.TabIndex = 1;
             this.Search.Text = "Search";
             this.Search.UseVisualStyleBackColor = true;
@@ -57,50 +69,101 @@ namespace FakeNewsDetectorApp
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(72, 270);
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.richTextBox1.BackColor = System.Drawing.Color.Silver;
+            this.richTextBox1.Location = new System.Drawing.Point(62, 179);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(524, 168);
+            this.richTextBox1.Size = new System.Drawing.Size(394, 176);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
-            // Thesaurus
+            // tabControl1
             // 
-            this.Thesaurus.Location = new System.Drawing.Point(998, 193);
-            this.Thesaurus.Name = "Thesaurus";
-            this.Thesaurus.Size = new System.Drawing.Size(76, 23);
-            this.Thesaurus.TabIndex = 3;
-            this.Thesaurus.Text = "Define";
-            this.Thesaurus.UseVisualStyleBackColor = true;
-            this.Thesaurus.Click += new System.EventHandler(this.Thesaurus_Click);
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(517, 407);
+            this.tabControl1.TabIndex = 0;
             // 
-            // textBox2
+            // tabPage1
             // 
-            this.textBox2.Location = new System.Drawing.Point(774, 132);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(524, 22);
-            this.textBox2.TabIndex = 4;
+            this.tabPage1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.tabPage1.Controls.Add(this.toolStrip1);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(509, 381);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
             // 
-            // richTextBox2
+            // toolStrip1
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(774, 270);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(524, 168);
-            this.richTextBox2.TabIndex = 5;
-            this.richTextBox2.Text = "";
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(509, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownButtonWidth = 0;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tEstToolStripMenuItem,
+            this.testToolStripMenuItem1});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(102, 22);
+            this.toolStripSplitButton1.Text = "Chee Mun dumb";
+            this.toolStripSplitButton1.ButtonClick += new System.EventHandler(this.toolStripSplitButton1_ButtonClick);
+            // 
+            // tEstToolStripMenuItem
+            // 
+            this.tEstToolStripMenuItem.Name = "tEstToolStripMenuItem";
+            this.tEstToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tEstToolStripMenuItem.Text = "TEst";
+            // 
+            // testToolStripMenuItem1
+            // 
+            this.testToolStripMenuItem1.Name = "testToolStripMenuItem1";
+            this.testToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.testToolStripMenuItem1.Text = "Test";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.tabPage2.Location = new System.Drawing.Point(4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(509, 381);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1352, 501);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.Thesaurus);
-            this.Controls.Add(this.richTextBox1);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(517, 407);
             this.Controls.Add(this.Search);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,9 +174,13 @@ namespace FakeNewsDetectorApp
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button Search;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button Thesaurus;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem tEstToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem1;
     }
 }
 
