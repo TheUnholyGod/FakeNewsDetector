e914e351d70b64a3316070f3a12c952705be21a8
HoopAWolf <hoopawolf394@gmail.com>
2018-07-14 02:42:51 +0800

    stuff
    stuff

---
+++ FakeNewsDetectorApp/FakeNewsDetectorApp/FileManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FakeNewsDetectorApp
+{
+    class FileManager
+    {
+        public static bool SaveToFile(string _filename,string[] _data)
+        {
+            if (_data.Length <= 0)
+                return false;
+            if(!File.Exists(_filename))
+            {
+                File.Create(_filename);
+                //while(!File.Exists(_filename))
+                //{
+
+                //}
+            }
+            foreach(string s in _data)
+            {
+                File.AppendAllText(_filename, s + "\n");
+            }
+            return true;
+        }
+
+        public static string[] LoadFromFile(string _filename)
+        {
+            string[] returnval = null;
+
+            if (!File.Exists(_filename))
+                throw new Exception("No File Found!");
+
+            returnval = File.ReadAllLines(_filename);
+            return returnval;
+        }
+    }
+}
