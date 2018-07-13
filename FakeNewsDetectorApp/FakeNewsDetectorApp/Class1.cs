dfd5ee7728c7e850cbb003b7e267aca2171dae31
TheUnholyGod <kitsonlow99@gmail.com>
2018-07-13 20:29:16 +0800

    Initial Commit

---
+++ FakeNewsDetectorApp/FakeNewsDetectorApp/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Services;
+
+namespace FakeNewsDetectorApp
+{
+    public class GoogleSearch
+    {
+        string apikey = "AIzaSyBy-_5XSuxpRnoaOVf6um2a6eD4q1lhkXU";
+        string searchengine = "001556608615224035032:lxiuybrekty";
+        CustomsearchService svc = null;
+
+        bool Init()
+        {
+            svc = new CustomsearchService(new BaseClientService.Initializer { ApiKey = apikey });
+            return true;
+        }
+
+        string[] Search(string _query)
+        {
+            CseResource.ListRequest listRequest = svc.Cse.List(_query);
+            listRequest.Cx = searchengine;
+            Search req = listRequest.Execute();
+            string[] ret = new string[req.Items.Count];
+            int i = 0;
+            foreach (Result result in req.Items)
+            {
+                ret[i] = result.Link;
+                ++i;
+            }
+            return ret;
+        }
+    }
+}
