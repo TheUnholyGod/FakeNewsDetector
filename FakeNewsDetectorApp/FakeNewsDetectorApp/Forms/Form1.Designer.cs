d20da65f4a01fa28c72332b6b6ae4ac5f9009755
TheUnholyGod <kitsonlow99@gmail.com>
2018-07-14 09:05:17 +0800

    Form Changes

--- FakeNewsDetectorApp/FakeNewsDetectorApp/Forms/Form1.Designer.cs
+++ FakeNewsDetectorApp/FakeNewsDetectorApp/Forms/Form1.Designer.cs
@@ -28,18 +28,23 @@ namespace FakeNewsDetectorApp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.x576ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x720ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.urlButton = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.toolStrip1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabControl1.SuspendLayout();
@@ -48,19 +53,51 @@ namespace FakeNewsDetectorApp
             // toolStrip1
             // 
             this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(677, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1334, 27);
             this.toolStrip1.TabIndex = 3;
             this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownButtonWidth = 0;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.x576ToolStripMenuItem,
+            this.x720ToolStripMenuItem});
+            this.toolStripSplitButton1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(69, 24);
+            this.toolStripSplitButton1.Text = "Window";
+            this.toolStripSplitButton1.ButtonClick += new System.EventHandler(this.toolStripSplitButton1_ButtonClick);
+            // 
+            // x576ToolStripMenuItem
+            // 
+            this.x576ToolStripMenuItem.Name = "x576ToolStripMenuItem";
+            this.x576ToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
+            this.x576ToolStripMenuItem.Text = "1024x576";
+            this.x576ToolStripMenuItem.Click += new System.EventHandler(this.x576ToolStripMenuItem_Click);
+            // 
+            // x720ToolStripMenuItem
+            // 
+            this.x720ToolStripMenuItem.Name = "x720ToolStripMenuItem";
+            this.x720ToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
+            this.x720ToolStripMenuItem.Text = "1280x720";
+            this.x720ToolStripMenuItem.Click += new System.EventHandler(this.x720ToolStripMenuItem_Click);
             // 
             // richTextBox3
             // 
             this.richTextBox3.BackColor = System.Drawing.SystemColors.InfoText;
+            this.richTextBox3.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.richTextBox3.ForeColor = System.Drawing.SystemColors.Info;
-            this.richTextBox3.Location = new System.Drawing.Point(-4, 472);
+            this.richTextBox3.Location = new System.Drawing.Point(0, 543);
             this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(1006, 32);
+            this.richTextBox3.Size = new System.Drawing.Size(1334, 32);
             this.richTextBox3.TabIndex = 4;
             this.richTextBox3.Text = "";
             // 
@@ -75,21 +112,36 @@ namespace FakeNewsDetectorApp
             this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Size = new System.Drawing.Size(669, 475);
+            this.tabPage2.Size = new System.Drawing.Size(1326, 487);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             // 
-            // richTextBox2
+            // button3
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(8, 8);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(665, 340);
-            this.richTextBox2.TabIndex = 0;
-            this.richTextBox2.Text = "";
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.Location = new System.Drawing.Point(349, 429);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Yes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.Location = new System.Drawing.Point(875, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "No";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(312, 382);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(613, 429);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -97,25 +149,14 @@ namespace FakeNewsDetectorApp
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // richTextBox2
             // 
-            this.button2.Location = new System.Drawing.Point(533, 426);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "No";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(84, 426);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Yes";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.richTextBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.richTextBox2.Location = new System.Drawing.Point(349, 28);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(601, 340);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
             // 
             // tabPage1
             // 
@@ -126,38 +167,40 @@ namespace FakeNewsDetectorApp
             this.tabPage1.Location = new System.Drawing.Point(4, 4);
             this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Size = new System.Drawing.Size(669, 475);
+            this.tabPage1.Size = new System.Drawing.Size(780, 487);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox2.BackColor = System.Drawing.Color.Silver;
+            this.textBox2.Location = new System.Drawing.Point(161, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(366, 22);
+            this.textBox2.TabIndex = 5;
             // 
             // richTextBox1
             // 
             this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.richTextBox1.BackColor = System.Drawing.Color.Silver;
-            this.richTextBox1.Location = new System.Drawing.Point(50, 220);
+            this.richTextBox1.Location = new System.Drawing.Point(157, 208);
             this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(400, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(469, 99);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
             // urlButton
             // 
-            this.urlButton.Location = new System.Drawing.Point(527, 73);
+            this.urlButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.urlButton.Location = new System.Drawing.Point(567, 73);
             this.urlButton.Name = "urlButton";
-            this.urlButton.Size = new System.Drawing.Size(68, 23);
+            this.urlButton.Size = new System.Drawing.Size(58, 26);
             this.urlButton.TabIndex = 4;
             this.urlButton.Text = "URL";
             this.urlButton.UseVisualStyleBackColor = true;
             this.urlButton.Click += new System.EventHandler(this.Search_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.Color.Silver;
-            this.textBox2.Location = new System.Drawing.Point(72, 73);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(377, 22);
-            this.textBox2.TabIndex = 5;
             // 
             // tabControl1
             // 
@@ -165,11 +208,11 @@ namespace FakeNewsDetectorApp
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.Location = new System.Drawing.Point(0, 27);
             this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(677, 504);
+            this.tabControl1.Size = new System.Drawing.Size(1334, 516);
             this.tabControl1.TabIndex = 0;
             // 
             // Form1
@@ -177,13 +220,15 @@ namespace FakeNewsDetectorApp
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(677, 529);
-            this.Controls.Add(this.richTextBox3);
+            this.ClientSize = new System.Drawing.Size(1334, 575);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.richTextBox3);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "y";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
@@ -206,6 +251,9 @@ namespace FakeNewsDetectorApp
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button urlButton;
         private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem x576ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x720ToolStripMenuItem;
     }
 }
 
